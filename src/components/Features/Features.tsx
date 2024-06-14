f9491fdabcb5a03f145b75adf36bf5a855d478a4
hamza01syed <127969573+hamza01syed@users.noreply.github.com>
2024-06-14 08:23:27 -0700

    image type changes to png

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import SectionHeading from '../SectionHeading/SectionHeading'
 import FeatureCard from './FeatureCard'
-import icon1 from "@/../public/ic5.jpg"
-import icon2 from "@/../public/ic1.jpg"
-import icon3 from "@/../public/ic2.jpg"
-import icon4 from "@/../public/ic3.jpg"
-import icon5 from "@/../public/ic4.jpg"
-import icon6 from "@/../public/ic5.jpg"
+// import icon1 from "@/../public/ic5.png"
+import icon1 from "@/../public/ic1.png"
+import icon2 from "@/../public/ic2.png"
+import icon4 from "@/../public/ic4.png"
+// import icon6 from "@/../public/ic5.png"
 
 
 const Features = () => {
@@ -29,15 +28,15 @@ const Features = () => {
             </div>
             <div data-aos="zoom-in" data-aos-delay="600" data-aos-anchor-placement="top-center">
                 {/* featre card  */}
-                <FeatureCard title="Email Subscription" image={icon4.src}/>
+                <FeatureCard title="Email Subscription" image={icon1.src}/>
             </div>
             <div data-aos="zoom-in" data-aos-delay="800" data-aos-anchor-placement="top-center">
                 {/* featre card  */}
-                <FeatureCard title="Email Subscription" image={icon5.src}/>
+                <FeatureCard title="Email Subscription" image={icon2.src}/>
             </div>
             <div data-aos="zoom-in" data-aos-delay="1000" data-aos-anchor-placement="top-center">
                 {/* featre card  */}
-                <FeatureCard title="Email Subscription" image={icon6.src}/>
+                <FeatureCard title="Email Subscription" image={icon4.src}/>
             </div>
         </div>
     </div>
