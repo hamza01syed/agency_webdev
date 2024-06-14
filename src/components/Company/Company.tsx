0fb35056a6990c00aa2c35084ff117393d89d793
hamza01syed <127969573+hamza01syed@users.noreply.github.com>
2024-06-14 08:46:20 -0700

    again png images added

--- src/components/Company/Company.tsx
+++ src/components/Company/Company.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import customer from "@/../public/Clients.jpg"
+import customer from "@/../public/Clients.png"
 
 const Company = () => {
   return (
